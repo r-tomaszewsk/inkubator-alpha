928e8829431e88f2fb19861b2f4078153cfb059d
tomaszpawlowski <t.pawlowski@live.com>
2019-03-20 11:48:36 +0100

    done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 document.getElementById("button").addEventListener("click", function(event){
   event.preventDefault();
 });
+document.getElementById("getModelButton").addEventListener("click", function(event){
+  event.preventDefault();
+});
