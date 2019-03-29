b85e873f2c6ca17ea8524254720f3077eb33ef2f
tomaszpawlowski <t.pawlowski@live.com>
2019-03-29 10:10:07 +0100

    prepering for player

--- builder.bat
+++ builder.bat
@@ -5,7 +5,7 @@ call cd export
 call tizen package -t wgt -s btproject -- ".buildResult"
 call mv .buildResult ..\.buildResult
 call cd ..
-call sdb connect 192.168.0.114
+call sdb connect 192.168.0.100
 call sdb push .buildResult\btproject.wgt /home/owner/share/tmp/sdk_tools/tmp/
 call sdb shell 0 vd_appinstall appis /home/owner/share/tmp/sdk_tools/tmp/btproject.wgt
 call sdb shell 0 debug Y63nG83NAY.btproject 300
---
+++ js/player.js
@@ -0,0 +1,30 @@
+function Player () {
+    //
+
+    document.addEventListener('visibilitychange', function () {
+        // 
+    });
+
+    function init (url) {
+        //
+    }
+
+    function play () {
+        //
+    }
+
+    function stop () {
+        //
+    }
+
+    function getStatus () {
+        //
+    }
+
+    return {
+        init: init,
+        play: play,
+        stop: stop,
+        getStatus: getStatus
+    };
+}
